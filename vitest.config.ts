df77aeeeeaa7d8603e0bd7c200e1a03735b33243
Kelek <kelekdev@gmail.com>
2025-02-03 14:15:15 +0300

    feat: add base config for vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,9 +6,16 @@ export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
+      globals: true,
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
+      coverage: {
+        provider: 'v8',
+        reporter: ['text', 'json', 'html'], // Форматы отчетов
+        include: ['src/**/*.{js,vue}'], // Включаем только файлы в папке src
+        exclude: ['src/main.js'], // Исключаем ненужные файлы
+      },
     },
   }),
 )
